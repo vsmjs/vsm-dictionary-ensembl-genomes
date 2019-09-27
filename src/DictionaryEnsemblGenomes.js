49d06f5c19af8830b1ab36863e3e61f9c8219470
john <bblodfon@gmail.com>
2019-09-27 15:03:47 +0200

    refactored filter dictID code

--- src/DictionaryEnsemblGenomes.js
+++ src/DictionaryEnsemblGenomes.js
@@ -48,29 +48,16 @@ module.exports = class DictionaryEnsemblGenomes extends Dictionary {
     if (!this.hasProperFilterIDProperty(options)) {
       return cb(null, res);
     } else {
-      // keep only the domain-specific dictID(s)
-      let idList = options.filter.id.filter(dictID =>
-        dictID.trim() === this.ensemblGenomesDictID
-      );
-
-      if (idList.length === 0) {
-        return cb(null, {items: []});
-      } else {
-        return cb(null, res);
-      }
+      return (options.filter.id.includes(this.ensemblGenomesDictID))
+        ? cb(null, res)
+        : cb(null, { items: [] });
     }
   }
 
   getEntries(options, cb) {
-    if (this.hasProperFilterDictIDProperty(options)) {
-      // keep only the domain-specific dictID(s)
-      let idList = options.filter.dictID.filter(dictID =>
-        dictID.trim() === this.ensemblGenomesDictID
-      );
-
-      if (idList.length === 0) {
-        return cb(null, { items: [] });
-      }
+    if (this.hasProperFilterDictIDProperty(options)
+      && !options.filter.dictID.includes(this.ensemblGenomesDictID)) {
+      return cb(null, { items: [] });
     }
 
     const url = this.prepareEntrySearchURL(options);
@@ -100,15 +87,9 @@ module.exports = class DictionaryEnsemblGenomes extends Dictionary {
   getEntryMatchesForString(str, options, cb) {
     if ((!str) || (str.trim() === '')) return cb(null, {items: []});
 
-    if (this.hasProperFilterDictIDProperty(options)) {
-      // keep only the domain-specific dictID(s)
-      let idList = options.filter.dictID.filter(dictID =>
-        dictID.trim() === this.ensemblGenomesDictID
-      );
-
-      if (idList.length === 0) {
-        return cb(null, { items: [] });
-      }
+    if (this.hasProperFilterDictIDProperty(options)
+      && !options.filter.dictID.includes(this.ensemblGenomesDictID)) {
+      return cb(null, { items: [] });
     }
 
     const url = this.prepareMatchStringSearchURL(str, options);
